6f58bab83915d3e35e57a8c8dd3256dfe6794809
Marius Stübs <marius.stuebs@riseup.net>
2014-01-31 22:07:07 +0100

    * Composer Window Address Bar: Bugfix Javascript

--- package/chrome/content/ui/messengerComposeOverlay.js
+++ package/chrome/content/ui/messengerComposeOverlay.js
@@ -2,12 +2,15 @@ Components.utils.import("chrome://storm/content/lib/global.jsm");
 Components.utils.import("chrome://storm/content/lib/utils.jsm");
 Components.utils.import("chrome://storm/content/lib/Keyring.jsm");
 
+/**
+ * Process the receivers email address, as soon as it changes
+ */
 $(window).load(function() {
     // AWESOME WORKAROUND EVENT INJECTION FOR CRAPPY EVENT DESIGN!
     // Appends the onInput function to the onInput attribute, because it somehow
     // does not work using $.on() -- doh!
     var widget = $(".textbox-addressingWidget");
-    widget.attr("oninput", widget.attr("oninput" + "; stormComposeAddressOverlayOnInput(this);");
+    widget.attr("oninput", widget.attr("oninput") + "; stormComposeAddressOverlayOnInput(this);");
 });
 
 // Overwrite the awAppendNewRow function, calling the original first, then settings
