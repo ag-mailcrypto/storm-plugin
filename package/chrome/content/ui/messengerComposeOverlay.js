9b6e64c5fa3ebe556b0f4805d627fe11969d2ce0
Niklas Wilcke <1wilcke@informatik.uni-hamburg.de>
2014-03-27 20:57:44 +0100

    compose overlay: new signed button, send button redesigned

--- package/chrome/content/ui/messengerComposeOverlay.js
+++ package/chrome/content/ui/messengerComposeOverlay.js
@@ -5,6 +5,11 @@ Components.utils.import("chrome://storm/content/lib/Keyring.jsm");
 Components.utils.import("chrome://storm/content/lib/MessageDraft.jsm");
 
 /**
+ * Register a Storm Message Draft as a global object
+ */
+messageDraftObject = new MessageDraft();
+
+/**
  * Process the receivers email address, as soon as it changes
  */
 $(window).load(function() {
@@ -13,11 +18,6 @@ $(window).load(function() {
     // does not work using $.on() -- doh!
     var widget = $(".textbox-addressingWidget");
     widget.attr("oninput", widget.attr("oninput") + "; stormComposeAddressOverlayOnInput(this);");
-
-    /**
-     * Register a Storm Message Draft as a global object
-     */
-    messageDraftObject = new MessageDraft();
     
     /**
      * Add an event listener that is triggered before sending an email.
@@ -38,6 +38,61 @@ $(window).load(function() {
         }, 
         true
     );
+    
+    // set window config
+    // signing
+    var sendSigned = messageDraftObject.getSendSigned();
+    $('#storm-button-sign').attr('checked', sendSigned);
+//    // encrypting
+    var sendEncrypted = messageDraftObject.getSendEncrypted();
+    storm.log("verschlüsselt senden? "+sendEncrypted);
+    if(sendEncrypted){
+    	$('#storm-button-send').attr("label", "Verschlüsselt senden");
+    	$('#storm-button-send').removeClass('send-unencrypted');
+    	$('#storm-button-send').addClass('send-encrypted');
+    	$('#storm-button-send-subbutton').attr("label", "Unverschlüsselt senden");
+    	$('#storm-button-send-subbutton').removeClass('send-encrypted');
+    	$('#storm-button-send-subbutton').addClass('send-unencrypted');
+    }else{
+    	$('#storm-button-send').attr("label","Unverschlüsselt senden");
+    	$('#storm-button-send').removeClass('send-encrypted');
+    	$('#storm-button-send').addClass('send-unencrypted');
+    	$('#storm-button-send-subbutton').attr("label", "Verschlüsselt senden");
+    	$('#storm-button-send-subbutton').removeClass('send-unencrypted');
+    	$('#storm-button-send-subbutton').addClass('send-encrypted');
+    }
+    
+    $('#storm-button-sign').click(function(){
+    	var state = $(this).attr('checked');
+    	messageDraftObject.setSendSigned(state);
+    	alert(messageDraftObject.getSendEncrypted());
+    });
+
+    var sendButtonPressed = function(button){
+    	
+    };
+
+    $('#storm-button-send').click(function(){
+    	var encryptMessage = $(this).hasClass("send-encrypted");
+    	if(encryptMessage){
+    		messageDraftObject.setSendEncrypted(true);
+    	}else{
+    		messageDraftObject.setSendEncrypted(false);
+    	}
+    });
+
+    $('#storm-button-send-subbutton').click(function(){
+    	var encryptMessage = $(this).hasClass("send-encrypted");
+    	if(encryptMessage){
+    		messageDraftObject.setSendEncrypted(true);
+    		alert(messageDraftObject.getSendEncrypted);
+    	}else{
+    		messageDraftObject.setSendEncrypted(false);
+    		alert(messageDraftObject.getSendEncrypted());
+    	}
+//    	sendButtonPressed(this);
+    });
+    
 });
 
 var setSendConfig = function(){
@@ -51,10 +106,10 @@ var toggleSignedCheckbox = function(){
     storm.log('toggle signed checkbox');
     var sendSigned = !messageDraftObject.getSendSigned();
     messageDraftObject.setSendSigned(sendSigned);
-}
+};
 
 var toggleEncryptCheckbox = function(){
     storm.log('toggle encrypted checkbox');
     var sendEncrypted = !messageDraftObject.getSendEncrypted();
     messageDraftObject.setSendEncrypted(sendEncrypted);
-}
+};
